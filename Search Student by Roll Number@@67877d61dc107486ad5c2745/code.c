2c7a3c485d89bdb0fdfdbc19e23af0f6104c3ad5
Upendra Rawat <rawatupendra689@gmail.com>
2025-03-11 16:50:12 +0530

    Solution to Search Student by Roll Number

--- Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
+++ Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
@@ -3,6 +3,12 @@
 int main(){
     struct student{
         int rollno;
-        char name
+        char name[50];
+        float marks;
+    }
+    int n;
+    scanf("%d", &n);
+    for (int i=0; i<n; i++){
+        scanf("%d", &struct)
     }
 }

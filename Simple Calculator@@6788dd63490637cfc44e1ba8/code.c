5dfe4b82e229c26cf3442b95d0b36b5772ac2a78
Upendra Rawat <rawatupendra689@gmail.com>
2025-03-14 16:08:05 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,6 +4,6 @@ int main(){
     int num1, num2;
     char oper;
     scanf("%d %d %c", &num1, &num2, &oper);
-    int value = num1 oper num2;
-    printf("%d", value);
+    int value = num1 num2 oper;
+    printf("%d %d %c", value);
 }

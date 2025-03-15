27c71518a202d5ff7f366f69e7d0e532bdd6b3d7
Upendra Rawat <rawatupendra689@gmail.com>
2025-03-16 00:20:46 +0530

    Solution to Toll Plaza Management System

---
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_VEHICLES 100
+
+struct Vehicle {
+    char vehicleNumber[20];
+    char vehicleType[10];
+    int tollAmount;
+};
+
+int main() {
+    int N;
+    scanf("%d", &N);  
+
+    struct Vehicle vehicles[MAX_VEHICLES];
+
+    int carsToll = 0, trucksToll = 0, bikesToll = 0;
+
+    for (int i = 0; i < N; i++) {
+        scanf("%s %s %d", vehicles[i].vehicleNumber, vehicles[i].vehicleType, &vehicles[i].tollAmount);
+
+        if (strcmp(vehicles[i].vehicleType, "Car") == 0) {
+            carsToll += vehicles[i].tollAmount;
+        } else if (strcmp(vehicles[i].vehicleType, "Truck") == 0) {
+            trucksToll += vehicles[i].tollAmount;
+        } else if (strcmp(vehicles[i].vehicleType, "Bike") == 0) {
+            bikesToll += vehicles[i].tollAmount;
+        }
+    }
+
+    printf("Cars: %d, Trucks: %d, Bikes: %d\n", carsToll, trucksToll, bikesToll);
+
+    return 0;
+}

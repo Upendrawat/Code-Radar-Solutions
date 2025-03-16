45be4285ee2b73d3903614d36e545ec9df35229e
Upendra Rawat <rawatupendra689@gmail.com>
2025-03-16 12:07:26 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -21,6 +21,7 @@ int main() {
 
     for (int i = 0; i < N; i++) {
         if (books[i].price > threshold) {
+            printf("Book aboce price %.2f\n", threshold);
             printf("Title: %s, Author: %s, Price: %.2f\n", books[i].title, books[i].author, books[i].price);
         }
     }

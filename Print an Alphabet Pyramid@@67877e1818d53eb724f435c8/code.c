a57b47f82d6a6666f0484b0f4f9d0a107840ae69
Upendra Rawat <rawatupendra689@gmail.com>
2025-03-16 13:17:46 +0530

    Solution to Print an Alphabet Pyramid

--- Print an Alphabet Pyramid@@67877e1818d53eb724f435c8/code.c
+++ Print an Alphabet Pyramid@@67877e1818d53eb724f435c8/code.c
@@ -8,7 +8,7 @@ int main() {
         for (int j = 1; j <= N - i; j++) {
             printf(" ");
         }
-        for (char j = 'A'; j <= 'A'+i; j++) {
+        for (char j = 'A'; j < 'A'+i; j++) {
             printf("%c ", j);
         }
         printf("\n");
